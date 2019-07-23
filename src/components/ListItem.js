02971781c8cc8da122ce72d7d86ef5710360f3de
Donata Mielaikaite <d.mielaikaite@gmail.com>
2019-07-23 21:41:59 +0200

    expanding the row

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,25 +1,54 @@
 import React, { Component } from 'react';
 import { CardSection } from './common';
-import { Text } from 'react-native';
+import { Text, View, TouchableWithoutFeedback } from 'react-native';
+import { connect } from 'react-redux';
+import { selectLibrary } from '../actions'
 
 class ListItem extends Component{
+
+  renderDescription() {
+    const { library, expanded } = this.props;
+
+    if (expanded){
+      return (
+        <Text>
+          {library.description}
+        </Text>
+      )
+    }
+  }
+
   render() {
     const { titleStyle } = styles;
+    const { title, id } = this.props.library;
+
     return(
-      <CardSection>
-        <Text style={titleStyle}>
-          {this.props.library.title}
-        </Text>
-      </CardSection>
+      <TouchableWithoutFeedback
+        onPress={() => this.props.selectLibrary(id)}
+      >
+        <View>
+          <CardSection>
+            <Text style={titleStyle}>
+              {title}
+            </Text>
+          </CardSection>
+          {this.renderDescription()}
+        </View>
+      </TouchableWithoutFeedback>
     );
   }
 }
 
 const styles = {
   titleStyle: {
-    fontSize: 18,
+    fontSize: 28,
     paddingLeft: 15
   }
 }
 
-export default ListItem;
+const mapStateToProps = (state, ownProps) => {
+  const expanded = state.selectedLibraryId === ownProps.library.id;
+  return { expanded }
+}
+
+export default connect(mapStateToProps, { selectLibrary })(ListItem);
