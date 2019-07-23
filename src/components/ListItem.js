f935b8cd8e542b734b478b6d1b8c984bd0977cc2
Donata Mielaikaite <d.mielaikaite@gmail.com>
2019-07-23 21:56:38 +0200

    added animations and componentWIllUpdate

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,25 +1,31 @@
 import React, { Component } from 'react';
 import { CardSection } from './common';
-import { Text, View, TouchableWithoutFeedback } from 'react-native';
+import { Text, View, TouchableWithoutFeedback, LayoutAnimation } from 'react-native';
 import { connect } from 'react-redux';
 import { selectLibrary } from '../actions'
 
 class ListItem extends Component{
 
+  componentWillUpdate() {
+    LayoutAnimation.spring();
+  }
+
   renderDescription() {
     const { library, expanded } = this.props;
-
+    const { descriptionStyle } = styles;
     if (expanded){
       return (
-        <Text>
-          {library.description}
-        </Text>
+        <CardSection>
+          <Text style={descriptionStyle}>
+            {library.description}
+          </Text>
+        </CardSection>
       )
     }
   }
 
   render() {
-    const { titleStyle } = styles;
+    const { titleStyle, descriptionStyle } = styles;
     const { title, id } = this.props.library;
 
     return(
@@ -43,6 +49,11 @@ const styles = {
   titleStyle: {
     fontSize: 28,
     paddingLeft: 15
+  },
+  descriptionStyle: {
+    paddingLeft: 15,
+    paddingRight: 15,
+    flex: 1
   }
 }
 
